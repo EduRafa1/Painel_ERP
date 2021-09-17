615aee6a4732112d9d089582cc6e2a33a9273001
EduRafa1 <youcodelove1@gmail.com>
2021-09-17 09:58:41 -0300

    1.1

--- painel/main.php
+++ painel/main.php
@@ -7,7 +7,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>|| PAINEL ERP ||</title>
+	<title>| PAINEL ERP |</title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 	<link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
